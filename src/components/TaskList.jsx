cbb0ea2f838dfd2e3f991ef6a45296a65198c052
rayyks <rayydna14@gmail.com>
2023-03-06 15:06:19 +0700

    add editing and deleting task

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -4,7 +4,7 @@ import TaskItem from "./TaskItem";
 // styles
 import styles from "./TaskList.module.css";
 
-const TaskList = ({ tasks, deleteTask, toggleTask }) => {
+const TaskList = ({ tasks, deleteTask, toggleTask, enterEditMode }) => {
   return (
     <ul className={styles.tasks}>
       {tasks
@@ -15,6 +15,7 @@ const TaskList = ({ tasks, deleteTask, toggleTask }) => {
             task={task}
             deleteTask={deleteTask}
             toggleTask={toggleTask}
+            enterEditMode={enterEditMode}
           />
         ))}
     </ul>
